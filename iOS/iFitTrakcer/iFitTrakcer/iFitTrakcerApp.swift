707dbbee5ed3ecbe12467b61351f7d3e0e9fbae1
Simeon Tsekov <simeon.1.tsekov@gmail.com>
2023-11-20 12:35:05 +0200

    Created empty iOS project

---
+++ iOS/iFitTrakcer/iFitTrakcer/iFitTrakcerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFitTrakcerApp.swift
+//  iFitTrakcer
+//
+//  Created by Simeon Tsekov on 20.11.23.
+//
+
+import SwiftUI
+
+@main
+struct iFitTrakcerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
