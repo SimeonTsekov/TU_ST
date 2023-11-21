8a71778378e7399dc7cdf04abc7459509e8d44e5
Antonia Ignatova <67511325+AntoniaIgn@users.noreply.github.com>
2023-11-21 23:09:25 +0200

    add user repository and controller

---
+++ Backend/webAPI/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        [HttpPost]
+        public IActionResult Create()
+        {
+
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id)
+        {
+
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult GetAllUsers()
+        {
+
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetUser(int id)
+        {
+
+        }
+    }
+}
+ 
---
+++ Backend/webAPI/Repository/UserRepository.cs
@@ -0,0 +1,58 @@
+﻿using webAPI.Data;
+using webApi.Data.Models;
+
+namespace webAPI.Repositories
+{
+    public class UserRepository
+    {
+        private readonly webAPIDbContext _dbContext;
+
+        public UserRepository(webAPIDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public UserModel Create(UserModel newUser)
+        {
+            _dbContext.UserModels.Add(newUser);
+            _dbContext.SaveChanges();
+            return newUser;
+        }
+
+        public UserModel Update(int userId, UserModel updatedUser)
+        {
+            var existingUser = _dbContext.UserModels.Find(userId);
+            if (existingUser != null)
+            {
+                existingUser.Username = updatedUser.Username;
+                existingUser.Email = updatedUser.Email;
+                existingUser.Passwords = updatedUser.Passwords;
+                existingUser.Age = updatedUser.Age;
+                existingUser.Height = updatedUser.Height;
+
+                _dbContext.SaveChanges();
+            }
+            return existingUser;
+        }
+
+        public void Delete(int userId)
+        {
+            var userToRemove = _dbContext.UserModels.Find(userId);
+            if (userToRemove != null)
+            {
+                _dbContext.UserModels.Remove(userToRemove);
+                _dbContext.SaveChanges();
+            }
+        }
+
+        public List<UserModel> GetAllUsers()
+        {
+            return _dbContext.UserModels.ToList();
+        }
+
+        public UserModel GetUserById(int userId)
+        {
+            return _dbContext.UserModels.Find(userId);
+        }
+    }
+}
