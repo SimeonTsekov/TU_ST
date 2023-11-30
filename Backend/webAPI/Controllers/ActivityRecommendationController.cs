4ab95ce4c9b47389178def857ba87b691b45ba1a
SvetoslavTaterski <svetoslav0247@gmail.com>
2023-11-30 15:39:19 +0200

    Adding ActivityRecommendationController

---
+++ Backend/webAPI/Controllers/ActivityRecommendationController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ActivityRecommendationController : Controller
+	{
+		[HttpPost]
+		public IActionResult Create()
+		{
+			throw new NotImplementedException();
+		}
+
+		[HttpPut("{id}")]
+		public IActionResult Update(int id)
+		{
+			throw new NotImplementedException();
+
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult Delete(int id)
+		{
+			throw new NotImplementedException();
+
+		}
+
+		[HttpGet]
+		public IActionResult GetAllActivityRecommendations()
+		{
+			throw new NotImplementedException();
+
+		}
+
+		[HttpGet("{id}")]
+		public IActionResult GetActivityRecommendation(int id)
+		{
+			throw new NotImplementedException();
+
+		}
+	}
+}
